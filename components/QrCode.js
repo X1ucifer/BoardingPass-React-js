bcdd3f201729b8821b445a88d63005a558005994
Akhil Jinu <portforwarding03@gmail.com>
2022-06-05 00:36:28 +0530

    final update1

--- components/QrCode.js
+++ components/QrCode.js
@@ -6,12 +6,164 @@ function QrCode() {
     return (
         <>
 
-            <div class="ticket">
-                
-                    <div class="qr">
-                        <QRCode style={{marginTop:"30px",marginBottom:"30px"}} value="https://boarding-pass-react-57ainw6ic-x1ucifer.vercel.app/Pass" size="200" renderAs="canvas" />
-                    </div>
-               
+            <div className='desktop'>
+
+                <div class="ticket">
+                    <header>
+                        <div class="company-name">
+                            Airlines
+                        </div>
+                        <div class="gate">
+                            <div>
+                                Gate
+                            </div>
+                            <div>
+                                B4
+                            </div>
+                        </div>
+                    </header>
+                    <section class="airports">
+                        <div class="airport">
+                            <div class="airport-name">
+                                Charlotte
+                            </div>
+                            <div class="airport-code">
+                                CLT
+                            </div>
+                            <div class="dep-arr-label">
+                                Departing
+                            </div>
+                            <div class="time">
+                                7:45am
+                            </div>
+                        </div>
+                        <div class="airport">
+                            <div class="airport-name">
+                                Tampa
+                            </div>
+                            <div class="airport-code">
+                                TPA
+                            </div>
+                            <div class="dep-arr-label">
+                                Arriving
+                            </div>
+                            <div class="time">
+                                9:15am
+                            </div>
+                        </div>
+                    </section>
+                    <section class="place">
+                        <div class="place-block">
+                            <div class="place-label">
+                                Group
+                            </div>
+                            <div class="place-value">
+                                2
+                            </div>
+                        </div>
+                        <div class="place-block">
+                            <div class="place-label">
+                                Seat
+                            </div>
+                            <div class="place-value">
+                                2A
+                            </div>
+                        </div>
+                        <div class="place-block">
+                            <div class="place-label">
+                                Term
+                            </div>
+                            <div class="place-value">
+                                B
+                            </div>
+                        </div>
+                        <div class="qr">
+                            <QRCode style={{ marginTop: "30px" }} value="https://boarding-pass-react-5y268zax1-x1ucifer.vercel.app/Pass" size="200" renderAs="canvas" />
+                        </div>
+                    </section>
+                </div>
+
+            </div>
+
+            {/* for mob */}
+
+            <div className='mob'>
+
+                <div class="ticket">
+                    <header>
+                        <div class="company-name">
+                            Airlines
+                        </div>
+                        <div class="gate">
+                            <div>
+                                Gate
+                            </div>
+                            <div>
+                                B4
+                            </div>
+                        </div>
+                    </header>
+                    <section class="airports">
+                        <div class="airport">
+                            <div class="airport-name">
+                                Charlotte
+                            </div>
+                            <div class="airport-code">
+                                CLT
+                            </div>
+                            <div class="dep-arr-label">
+                                Departing
+                            </div>
+                            <div class="time">
+                                7:45am
+                            </div>
+                        </div>
+                        <div class="airport">
+                            <div class="airport-name">
+                                Tampa
+                            </div>
+                            <div class="airport-code">
+                                TPA
+                            </div>
+                            <div class="dep-arr-label">
+                                Arriving
+                            </div>
+                            <div class="time">
+                                9:15am
+                            </div>
+                        </div>
+                    </section>
+                    <section class="place">
+                        <div class="place-block">
+                            <div class="place-label">
+                                Group
+                            </div>
+                            <div class="place-value">
+                                2
+                            </div>
+                        </div>
+                        <div class="place-block">
+                            <div class="place-label">
+                                Seat
+                            </div>
+                            <div class="place-value">
+                                2A
+                            </div>
+                        </div>
+                        <div class="place-block">
+                            <div class="place-label">
+                                Term
+                            </div>
+                            <div class="place-value">
+                                B
+                            </div>
+                        </div>
+                        <div class="qr">
+                            <QRCode style={{ marginTop: "30px",marginLeft:"10%" }} value="https://boarding-pass-react-5y268zax1-x1ucifer.vercel.app/Pass" size="50" renderAs="canvas" />
+                        </div>
+                    </section>
+                </div>
+
             </div>
 
         </>
